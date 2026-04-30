68197def849fa0e6bfa985d4a950642849728527
prudviraj12 <prudvirajreddy98@gmail.com>
2026-04-30 14:48:15 +0530

    prog

--- python_rev/qns.py
+++ python_rev/qns.py
@@ -1,18 +1,18 @@
-def invert_dict(d):
-    di = {}
-    for k, v in d.items():
-        di[v] = k
-    return di
+# def invert_dict(d):
+#     di = {}
+#     for k, v in d.items():
+#         di[v] = k
+#     return di
 
-n = int(input("len of dict: "))
-data = {}
+# n = int(input("len of dict: "))
+# data = {}
 
-for i in range(n):
-    k = input("enter key: ")
-    v = int(input("enter val: "))
-    data[k] = v
+# for i in range(n):
+#     k = input("enter key: ")
+#     v = int(input("enter val: "))
+#     data[k] = v
 
-print("original:", data)
+# print("original:", data)
 
-res = invert_dict(data)
-print("res:", res)
+# res = invert_dict(data)
+# print("res:", res)
